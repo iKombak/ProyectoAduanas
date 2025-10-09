5ef2e06394535b8ce312d47b69c6ec907aa2aa5d
iKombak <enzocomba38@gmail.com>
2025-10-09 09:30:44 -0300

    subir

---
+++ ParkingAduanas/src/main/java/com/mycompany/parkingaduanas/ParkingAduanas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parkingaduanas;
+
+/**
+ *
+ * @author enzo
+ */
+public class ParkingAduanas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
