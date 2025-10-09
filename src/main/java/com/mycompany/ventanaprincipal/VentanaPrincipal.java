fb4d56c36d868c18875aadc6999a71b544d50161
Enzo <enzocomba38@gmail.com>
2025-10-09 08:57:06 -0300

    first commit

---
+++ src/main/java/com/mycompany/ventanaprincipal/VentanaPrincipal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ventanaprincipal;
+
+/**
+ *
+ * @author enzo
+ */
+public class VentanaPrincipal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Ventana_Menu princ = new Ventana_Menu();
+        princ.setVisible(true);
+        
+        
+    }
+}
